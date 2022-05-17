fb2e081a972bc452200a9da7bac299794555f4e7
Jesse-Driskill <jesse@driskill.com>
2022-05-16 17:27:07 -0700

    sampletext

---
+++ frontend/components/app.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+
+
+
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        <div>
+            Hello!!!!
+        </div>
+    }
+}
+
+export default App;
---
+++ frontend/components/root.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { Provder } from 'react-redux';
+import { HashRouter } from "react-router-dom";
+import App from './app';
+
+class Root extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        <App></App>
+    }
+
+    
+}
+
+export default Root;
